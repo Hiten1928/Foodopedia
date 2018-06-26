a140801ef72e02e964841ab5c5fd8a6972d93441
Hiten1928 <hg1928@rit.edu>
2018-06-26 18:05:25 -0400

    Completed ViewComponent

---
+++ ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeeter)
+        {
+            _greeter = greeeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetMessageOfTheday();
+            return View("Default", model);
+        }
+    }
+}
