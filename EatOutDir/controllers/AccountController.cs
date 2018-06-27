6420b3db8ca0ca8a76afd453bbd697e5a1f075ec
Hiten1928 <hg1928@rit.edu>
2018-06-27 11:32:39 -0400

    added Authentication schema for logout

--- EatOutDir/controllers/AccountController.cs
+++ EatOutDir/controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -16,6 +17,18 @@ namespace OdeToFood.controllers
             return Challenge(
                 new AuthenticationProperties { RedirectUri = redirectUrl },
                 //Challenging the user by logging in with the OIDC server (Azure)
+                OpenIdConnectDefaults.AuthenticationScheme
+                );
+        }
+
+        public IActionResult Logout()
+        {
+            var callbackUrl = Url.Page("/Account/SignedOut");
+            return SignOut(
+                new AuthenticationProperties { RedirectUri = callbackUrl },
+                // remove cookie from the list which is associated to the user
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                // also logout of OIDC identity provider
                 OpenIdConnectDefaults.AuthenticationScheme
                 );
         }
