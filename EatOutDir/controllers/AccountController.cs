a90221382e5d8e7fc627716272859bbc26be5a8a
Hiten1928 <hg1928@rit.edu>
2018-06-27 11:27:40 -0400

    aded Sign in, sign out

---
+++ EatOutDir/controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            var redirectUrl = Url.Page("/Index");
+            return Challenge(
+                new AuthenticationProperties { RedirectUri = redirectUrl },
+                //Challenging the user by logging in with the OIDC server (Azure)
+                OpenIdConnectDefaults.AuthenticationScheme
+                );
+        }
+    }
+}
